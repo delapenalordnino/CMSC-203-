6a10ef1ce5cdd1907398bcba53f7d492b49aefc7
Lord Niño L. Dela Peña <112452625+delapenalordnino@users.noreply.github.com>
2022-09-13 18:30:06 +0800

    Add files via upload

---
+++ CMSC203_ACTIVITY2_DELAPEÑA/sentencebuilder.java
@@ -0,0 +1,14 @@
+public class sentencebuilder {
+    public static void main(String[] args)
+    {
+        String name = "Lord Niño Dela Peña";
+        int age = 19;
+        float GWA = 1.56f;
+        char myBloodtype = 'O';
+        System.out.println("Hi, my name is " + name + ".");
+        System.out.println("I am " + age + " years old.");
+        System.out.println("My GWA is " + GWA + ".");
+        System.out.println("My blood type is " + myBloodtype + ".");
+    }
+}
+
